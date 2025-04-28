fd9a286c69159ada038ff839e9dbdba5a3ef872a
Stefan Cristian B. <stefan.cristian+git@rogentos.ro>
2025-04-28 09:24:44 +0300

    app-misc/calamares-config-livecd: always detect newest stage3 amd64 openrc
    Signed-off-by: Stefan Cristian B. <stefan.cristian+git@rogentos.ro>

--- app-misc/calamares-config-livecd/files/modules/downloadstage3/main.py
+++ app-misc/calamares-config-livecd/files/modules/downloadstage3/main.py
@@ -6,17 +6,61 @@ import tarfile
 import subprocess
 import libcalamares
 import glob
+import re
+import sys
+import time
 
 def _progress_hook(count, block_size, total_size):
+    _check_parent_alive()
     percent = int(count * block_size * 100 / total_size)
     if percent > 100:
         percent = 100
     libcalamares.job.setprogress(percent / 2)
 
+def _check_parent_alive():
+    if os.getppid() == 1:
+        sys.exit(1)
+
+def _find_latest_stage3(url):
+    with urllib.request.urlopen(url) as response:
+        html = response.read().decode('utf-8')
+    matches = re.findall(r'(stage3-\w+-openrc-\d+T\d+Z\.tar\.xz)', html)
+    matches = sorted(matches)
+    if matches:
+        return matches[-1]
+    return None
+
+def _safe_run(cmd):
+    _check_parent_alive()
+    try:
+        proc = subprocess.Popen(cmd)
+        while True:
+            retcode = proc.poll()
+            if retcode is not None:
+                return retcode
+            if os.getppid() == 1:
+                proc.terminate()
+                try:
+                    proc.wait(timeout=5)
+                except subprocess.TimeoutExpired:
+                    proc.kill()
+                sys.exit(1)
+            time.sleep(1)
+    except subprocess.SubprocessError:
+        pass
+
 def run():
-    url = "https://distfiles.gentoo.org/releases/amd64/autobuilds/20250420T121009Z/stage3-amd64-openrc-20250420T121009Z.tar.xz"
-    download_path = "/tmp/stage3.tar.xz"
-    extract_path = "/tmp/gentoo-rootfs"
+    base_url = "https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-openrc/"
+    latest_tarball = _find_latest_stage3(base_url)
+    if not latest_tarball:
+        raise Exception("No stage3 tar.xz found!")
+
+    full_tarball_url = base_url + latest_tarball
+    full_digest_url = base_url + latest_tarball + ".DIGESTS"
+
+    download_path = f"/tmp/{latest_tarball}"
+    digest_path = f"/tmp/{latest_tarball}.DIGESTS"
+    extract_path = "/mnt/gentoo-rootfs"
 
     if os.path.exists(extract_path):
         for entry in os.listdir(extract_path):
@@ -30,52 +74,61 @@ def run():
 
     if os.path.exists(download_path):
         os.remove(download_path)
+    if os.path.exists(digest_path):
+        os.remove(digest_path)
 
-    urllib.request.urlretrieve(url, download_path, _progress_hook)
+    urllib.request.urlretrieve(full_tarball_url, download_path, _progress_hook)
+    libcalamares.job.setprogress(40)
+    urllib.request.urlretrieve(full_digest_url, digest_path)
     libcalamares.job.setprogress(50)
 
     with tarfile.open(download_path, "r:xz") as tar:
         members = tar.getmembers()
         total_members = len(members)
         for i, member in enumerate(members):
+            _check_parent_alive()
             tar.extract(member, extract_path)
             libcalamares.job.setprogress(50 + (i * 50 / total_members))
 
+    os.remove(download_path)
+    os.remove(digest_path)
+
     shutil.copy2("/etc/resolv.conf", os.path.join(extract_path, "etc", "resolv.conf"))
+    _safe_run(["chroot", extract_path, "getuto"])
 
     package_use_dir = os.path.join(extract_path, "etc/portage/package.use")
     os.makedirs(package_use_dir, exist_ok=True)
     with open(os.path.join(package_use_dir, "00-livecd.package.use"), "w", encoding="utf-8") as f:
         f.write(">=sys-kernel/installkernel-50 dracut\n")
 
-    subprocess.run(["mount", "--bind", "/proc", os.path.join(extract_path, "proc")], check=True)
-    subprocess.run(["mount", "--bind", "/sys", os.path.join(extract_path, "sys")], check=True)
-    subprocess.run(["mount", "--bind", "/dev", os.path.join(extract_path, "dev")], check=True)
-    subprocess.run(["mount", "--bind", "/run", os.path.join(extract_path, "run")], check=True)
+    _safe_run([
+        "git", "clone", "--depth=1",
+        "https://github.com/gentoo/gentoo.git",
+        os.path.join(extract_path, "var/db/repos/gentoo")
+    ])
 
-    try:
-        subprocess.run(["chroot", extract_path, "emerge", "--sync"], check=True)
-    except subprocess.CalledProcessError:
-        pass
+    _safe_run(["mount", "--bind", "/proc", os.path.join(extract_path, "proc")])
+    _safe_run(["mount", "--bind", "/sys", os.path.join(extract_path, "sys")])
+    _safe_run(["mount", "--bind", "/dev", os.path.join(extract_path, "dev")])
+    _safe_run(["mount", "--bind", "/run", os.path.join(extract_path, "run")])
 
-    try:
-        subprocess.run(["chroot", extract_path, "emerge", "-v", "sys-apps/dbus", "sys-boot/grub", "sys-kernel/gentoo-kernel-bin"], check=True)
-    except subprocess.CalledProcessError:
-        pass
+    _safe_run([
+        "chroot", extract_path, "/bin/bash", "-c",
+        'EMERGE_DEFAULT_OPTS="${EMERGE_DEFAULT_OPTS} --getbinpkg" emerge -q sys-apps/dbus sys-boot/grub'
+    ])
 
-    subprocess.run(["rm", "-rf", os.path.join(extract_path, "var/db/repos/gentoo")])
+    for folder in ["distfiles", "binpkgs"]:
+        path = os.path.join(extract_path, f"var/cache/{folder}")
+        if os.path.exists(path):
+            for entry in glob.glob(path + "/*"):
+                if os.path.isfile(entry) or os.path.islink(entry):
+                    os.unlink(entry)
+                elif os.path.isdir(entry):
+                    shutil.rmtree(entry)
 
-    distfiles_path = os.path.join(extract_path, "var/cache/distfiles")
-    if os.path.exists(distfiles_path):
-        for entry in glob.glob(distfiles_path + "/*"):
-            if os.path.isfile(entry) or os.path.islink(entry):
-                os.unlink(entry)
-            elif os.path.isdir(entry):
-                shutil.rmtree(entry)
-
-    subprocess.run(["umount", "-l", os.path.join(extract_path, "proc")])
-    subprocess.run(["umount", "-l", os.path.join(extract_path, "sys")])
-    subprocess.run(["umount", "-l", os.path.join(extract_path, "dev")])
-    subprocess.run(["umount", "-l", os.path.join(extract_path, "run")])
+    _safe_run(["umount", "-l", os.path.join(extract_path, "proc")])
+    _safe_run(["umount", "-l", os.path.join(extract_path, "sys")])
+    _safe_run(["umount", "-l", os.path.join(extract_path, "dev")])
+    _safe_run(["umount", "-l", os.path.join(extract_path, "run")])
 
     return None
