5631ca9dd7c57f79ec411e90f618915f32db3841
Stefan Cristian B. <stefan.cristian+git@rogentos.ro>
2025-04-30 05:18:43 +0300

    app-misc/calamares-config-livecd: added support for stage3 download and install
    Signed-off-by: Stefan Cristian B. <stefan.cristian+git@rogentos.ro>

--- app-misc/calamares-config-livecd/calamares-config-livecd-3.3.14.ebuild
+++ app-misc/calamares-config-livecd/calamares-config-livecd-3.3.14.ebuild
@@ -6,8 +6,9 @@ EAPI="8"
 
 MY_PN="calamares"
 MY_P="${MY_PN}-${PV}"
+PYTHON_COMPAT=( python3_{11..13} )
 
-inherit desktop
+inherit desktop python-single-r1
 
 DESCRIPTION="Gentoo Linux ${MY_PN} installer config"
 HOMEPAGE="https://wiki.gentoo.org/wiki/No_homepage"
@@ -18,42 +19,56 @@ S="${WORKDIR}"
 LICENSE="GPL-2"
 SLOT="0"
 KEYWORDS="~amd64"
-RDEPEND="app-admin/calamares"
+DEPEND="
+    ${PYTHON_DEPS}
+    dev-python/pygobject:3
+    x11-libs/gtk+:3
+    x11-libs/gdk-pixbuf
+    x11-libs/pango
+"
+RDEPEND="${DEPEND}
+		app-admin/calamares"
 
 src_prepare() {
-    default
+	default
 
-    local baselayout_version
+	local baselayout_version
 	baselayout_version=$(best_version sys-apps/baselayout)
 	baselayout_version=${baselayout_version#*layout-}
 
-    cp "${FILESDIR}/artwork/branding.desc" "${WORKDIR}/branding.desc" || die
+	cp "${FILESDIR}/artwork/branding.desc" "${WORKDIR}/branding.desc" || die
 
-    if [[ -n ${baselayout_version} ]]; then
-        sed -i "s|GENTOO_VERSION|${baselayout_version}|g" "${WORKDIR}/branding.desc" || die
-    fi
+	if [[ -n ${baselayout_version} ]]; then
+		sed -i "s|GENTOO_VERSION|${baselayout_version}|g" "${WORKDIR}/branding.desc" || die
+	fi
 }
 
 src_install() {
 	insinto "/etc/${MY_PN}"
-	doins -r ${FILESDIR}/modules/
-	doins -r ${FILESDIR}/settings.conf
+	doins -r "${FILESDIR}"/settings.conf
+
+	insinto "/etc/${MY_PN}/modules"
+	for conf_file in "${FILESDIR}"/modules/*.conf; do
+		newins "${conf_file}" "$(basename "${conf_file}")"
+	done
 
 	insinto /usr/$(get_libdir)/calamares/modules/
-	doins -r ${FILESDIR}/modules/downloadstage3
-	doins -r ${FILESDIR}/modules/dracut_gentoo
+	doins -r "${FILESDIR}"/modules/downloadstage3
+	doins -r "${FILESDIR}"/modules/dracut_gentoo
+	doins -r "${FILESDIR}"/modules/stage3chooser
 
-	domenu ${FILESDIR}/gentoo-installer.desktop
+	domenu "${FILESDIR}"/gentoo-installer.desktop
 
 	insinto /usr/bin/
-	dobin "${FILESDIR}"/${MY_PN}-pkexec
+	dobin "${FILESDIR}/${MY_PN}-pkexec"
+	dobin "${FILESDIR}"/stage3chooser.py
 
 	insinto /usr/share/icons/calamares-gentoo/64x64/
 	newins gentoo-artwork-0.2/icons/gentoo/64x64/gentoo.png gentoo.png
 
 	insinto /etc/calamares/branding/gentoo_branding
-	doins -r ${FILESDIR}/artwork/show.qml
-	doins -r ${WORKDIR}/branding.desc
+	doins -r "${FILESDIR}"/artwork/show.qml
+	doins -r "${WORKDIR}"/branding.desc
 
 	local i
 	for i in {1..10}; do
--- app-misc/calamares-config-livecd/files/modules/downloadstage3/main.py
+++ app-misc/calamares-config-livecd/files/modules/downloadstage3/main.py
@@ -9,6 +9,8 @@ import glob
 import re
 import sys
 import time
+from urllib.parse import urljoin
+from datetime import datetime
 
 def _progress_hook(count, block_size, total_size):
     _check_parent_alive()
@@ -21,14 +23,24 @@ def _check_parent_alive():
     if os.getppid() == 1:
         sys.exit(1)
 
-def _find_latest_stage3(url):
+def _find_latest_stage3(url, stage3_value):
     with urllib.request.urlopen(url) as response:
         html = response.read().decode('utf-8')
-    matches = re.findall(r'(stage3-\w+-openrc-\d+T\d+Z\.tar\.xz)', html)
-    matches = sorted(matches)
-    if matches:
-        return matches[-1]
-    return None
+    print("DEBUG HTML:", html)
+    base_pattern = stage3_value.replace("current-", "")
+    pattern = rf"{base_pattern}-(\d{{8}}T\d{{6}})Z\.tar\.xz\b(?!\.)"
+    print("DEBUG pattern:", pattern)
+    matches = re.findall(pattern, html)
+    print("DEBUG matches:", matches)
+    if not matches:
+        raise Exception("No valid .tar.xz files found.")
+    timestamps = [datetime.strptime(ts, "%Y%m%dT%H%M%S") for ts in matches]
+    latest_ts = max(timestamps)
+    latest_ts_str = latest_ts.strftime("%Y%m%dT%H%M%S")
+    latest_filename = f"{base_pattern}-{latest_ts_str}Z.tar.xz"
+    full_url = urljoin(url, latest_filename)
+    print(f"DEBUG Full URL: {full_url}")
+    return latest_filename
 
 def _safe_run(cmd):
     _check_parent_alive()
@@ -52,8 +64,47 @@ def _safe_run(cmd):
         sys.exit(1)
 
 def run():
-    base_url = "https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-openrc/"
-    latest_tarball = _find_latest_stage3(base_url)
+    stage3_types = {
+        "current-stage3-amd64-desktop-openrc",
+        "current-stage3-amd64-desktop-systemd",
+        "current-stage3-amd64-hardened-openrc",
+        "current-stage3-amd64-hardened-selinux-openrc",
+        "current-stage3-amd64-hardened-systemd",
+        "current-stage3-amd64-llvm-openrc",
+        "current-stage3-amd64-llvm-systemd",
+        "current-stage3-amd64-musl-hardened",
+        "current-stage3-amd64-musl-llvm",
+        "current-stage3-amd64-musl",
+        "current-stage3-amd64-nomultilib-openrc",
+        "current-stage3-amd64-nomultilib-systemd",
+        "current-stage3-amd64-openrc-splitusr",
+        "current-stage3-amd64-openrc",
+        "current-stage3-amd64-systemd",
+        "current-stage3-x32-openrc",
+        "current-stage3-x32-systemd"
+    }
+
+    stage3_value = "current-stage3-amd64-openrc"
+
+    if os.path.exists("/etc/calamares.conf"):
+        with open("/etc/calamares.conf", "r", encoding="utf-8") as f:
+            for line in f:
+                match = re.match(r'^\s*stage3\s*=\s*"([^"]+)"\s*$', line)
+                if match:
+                    val = match.group(1)
+                    if val in stage3_types:
+                        stage3_value = val
+                        break
+                    else:
+                        raise Exception(f"Invalid stage3 value in /etc/calamares.conf: {val}")
+
+    arch_match = re.search(r'current-stage3-([^-]+)', stage3_value)
+    if not arch_match:
+        raise Exception(f"Could not determine arch from stage3 value: {stage3_value}")
+    arch = arch_match.group(1)
+
+    base_url = f"https://distfiles.gentoo.org/releases/{arch}/autobuilds/{stage3_value}/"
+    latest_tarball = _find_latest_stage3(base_url, stage3_value)
     if not latest_tarball:
         raise Exception("No stage3 tar.xz found!")
 
